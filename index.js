c8befa2fa5d52f8a73b047b4a60237b431244769
Elian Ruiz <elianruizpedraza@gmail.com>
2023-01-25 09:12:12 -0500

    Se realiza conexion a servidor NODE y se configura los CORS

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const { connection } = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
 
 // Inicializacion de la app
 console.log('Aplicacion de Node esta Funcionando!');
 
 // Conectar a la Base de Datos
 connection();
+
+// Crear Servidor de Node
+const app = express();
+const port = 3900;
+
+// Configuracion de CORS
+app.use(cors());
+
+// Convertir Body a objeto js
+app.use(express.json());
+
+// Crear rutas
+
+
+// Crear Servidor y escuchar peticiones http
+app.listen( port, () => {
+    console.log(`Servidor Corriendo en el puerto: ${port}`);
+});
